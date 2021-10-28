54153e321d8e21b07657dad57b105b766874cf6d
cschang <j06292001@gmail.com>
2021-10-28 12:04:02 +0800

    Updates

---
+++ precache-manifest.276f52caa3390090855dc48b7ae97daa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0120f54819fd9a93ae0801463d1ff346",
+    "url": "/crwn-clothing-impl/index.html"
+  },
+  {
+    "revision": "059f7c1a04155324653e",
+    "url": "/crwn-clothing-impl/static/css/main.d6405643.chunk.css"
+  },
+  {
+    "revision": "5ca336898b7f2a182daa",
+    "url": "/crwn-clothing-impl/static/js/2.9a988375.chunk.js"
+  },
+  {
+    "revision": "059f7c1a04155324653e",
+    "url": "/crwn-clothing-impl/static/js/main.8ac8609d.chunk.js"
+  },
+  {
+    "revision": "33b92f73ccc7f2fb2a38",
+    "url": "/crwn-clothing-impl/static/js/runtime~main.a21ea936.js"
+  },
+  {
+    "revision": "19ef7b17bc9414c63c7781251ce0d24d",
+    "url": "/crwn-clothing-impl/static/media/crown.19ef7b17.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/crwn-clothing-impl/static/media/shopping-bag.30f07071.svg"
+  }
+]);
